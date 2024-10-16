1cb7cf9c4b951e264494db6e315ef7e6c954e981
khang-code <trieukhang2006@gmail.com>
2024-10-16 09:16:55 +0700

    Add files via upload

--- code/QuanLyDonHang.sql
+++ code/QuanLyDonHang.sql
@@ -4,13 +4,13 @@ use QuanLyBanHang;
 create table Customer(
 cID int not null auto_increment primary key,
 cName varchar(50),
-cAge int
+cAge tinyint
 );
 
 create table Orders(
 oID int auto_increment primary key,
 cID int not null,
-oDate date,
+oDate datetime,
 oTotalePrice int,
 foreign key (cID) references Customer (cID)
 );
@@ -23,6 +23,7 @@ pPrice int);
 create table OrderDetail (
 oID int not null,
 pID int not null,
+odQuanity int,
 foreign key (oID) references Orders (oID),
 foreign key (pID) references Product (pID)
-)
+);
